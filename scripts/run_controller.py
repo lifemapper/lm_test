fa0fc2301b0fb5a7e504da04db7f5549ffa1e92b
CJ Grady <cjgrady@gmail.com>
2021-03-17 11:40:35 -0500

    Add lm_client test

--- scripts/run_controller.py
+++ scripts/run_controller.py
@@ -6,10 +6,13 @@ from lm_test.base.daemon import DaemonCommands
 from lm_test.base.controller import Controller, CONTROLLER_PID_FILE
 from lm_test.tests.disk_usage_test import DiskUsageTest
 from lm_test.tests.memory_usage_test import MemoryUsageTest
+from lm_test.tests.lm_client_test import LmClientTest
 
 # Note: Edit this for now.  We will need a better mechanism for adding tests
 TESTS_TO_RUN = [
-    MemoryUsageTest(1, 99, delay_interval=60),
+    #PytestTest('/home/cjgrady/git/lm_client/'),
+    LmClientTest(delay_interval=600),
+    MemoryUsageTest(1, 99, delay_interval=600),
     DiskUsageTest('/DATA/', 20, 80, delay_interval=3600),
     DiskUsageTest('/', 10, 90, delay_interval=3600)
 ]
